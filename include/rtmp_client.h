6e9026d529a1f0a04b3d71f5e46ccf52e2dff0f5
Andrew Story <kaslai@kaslai.com>
2016-07-27 20:09:23 -0700

    Add header for RTMP Client

--- include/rtmp_client.h
+++ include/rtmp_client.h
@@ -24,3 +24,48 @@
 #pragma once
 
 #include "rtmp/rtmp_types.h"
+
+typedef struct rtmp_client * rtmp_client_t;
+
+rtmp_client_t rtmp_client_create( void );
+void rtmp_client_destroy( rtmp_client_t client );
+
+//See NetConnect documentation for usage
+rtmp_err_t rtmp_client_call( rtmp_client_t client, const char *name, double id, ... );
+rtmp_err_t rtmp_client_respond( rtmp_client_t client, const char *name, double id, ... );
+rtmp_err_t rtmp_client_connect( rtmp_client_t client, ... );
+
+rtmp_err_t rtmp_client_play( rtmp_client_t client,
+    const char *stream_name,
+    double start DEFAULT(-1),
+    double duration DEFAULT(-1),
+    bool reset DEFAULT(false) );
+
+rtmp_err_t rtmp_client_play2( rtmp_client_t client,
+    const char * old_stream_name,
+    const char * new_stream_name,
+    double start_time,
+    double offset,
+    double len,
+    const char *restrict transition );
+
+rtmp_err_t rtmp_client_deletestream( rtmp_client_t client,
+    double stream_id);
+
+rtmp_err_t rtmp_client_recv_audio( rtmp_client_t client,
+    bool want_audio);
+
+rtmp_err_t rtmp_client_recv_video( rtmp_client_t client,
+    bool want_video);
+
+rtmp_err_t rtmp_client_publish( rtmp_client_t client,
+    const char * restrict publish_name,
+    const char * restrict publish_type);
+
+rtmp_err_t rtmp_client_seek( rtmp_client_t client,
+    rtmp_time_t seek_ms);
+
+rtmp_err_t rtmp_client_pause( rtmp_client_t client,
+    bool paused,
+    rtmp_time_t pause_ms);
+
